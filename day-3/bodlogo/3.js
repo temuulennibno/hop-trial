b812222c4e7af086e49ea53c2602ef2e8345a381
Temuulen Narmandakh <temuulen.n@pinecone.mn>
2024-06-01 12:37:15 +0800

    exercise order

--- day-3/bodlogo/3.js
+++ day-3/bodlogo/3.js
@@ -1,7 +1,17 @@
 // haich chuluu daavuu
 
 const player1 = "haich";
-const player2 = "daavuu";
+const player2 = "haich";
 
 // hojson toglogchiig hevleh
 // player1 wins
+
+if (player1 === player2) {
+  console.log("tie");
+} else if (player1 === "haich") {
+  if (player2 === "daavuu") {
+    console.log("player1 wins");
+  } else if (player2 === "chuluu") {
+    console.log("player2 wins");
+  }
+}
