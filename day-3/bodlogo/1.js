a104b8f3663aa30b832e3bd82d23af288ff29eed
Temuulen Narmandakh <temuulen.n@pinecone.mn>
2024-06-01 12:20:18 +0800

    day3 exercises

--- day-3/bodlogo/1.js
+++ day-3/bodlogo/1.js
@@ -1,6 +1,11 @@
 // delguur humuusiin nernees hamaarch 7 honog uramshuulal
 //zarlasan ba 1 deh odor nerend ni davaa orson humuus
 //hymdaraltai nerend ni myagmar orson humuus 2doh odor hymdraltai
+// lkhagva - 3dah odor
+// purev - 4dah odor
+// baasan - 5dah odor
+// byamba - 6dah odor
+// nyam - 7dah odor
 const name = "dashdulam";
 const dayofweek = 1;
 
